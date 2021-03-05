8451a83f33ca96594aebf91ed4938e03a6578035
Grace Lindelien <gracelindelien@gmail.com>
2021-03-05 09:57:11 -0800

    Render Grudges component in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Title from './components/Title.js';
 import AddGrudge from './components/AddGrudge.js';
+import Grudges from './components/Grudges.js';
 
 function App() {
   return (
     <>
       <Title />
       <AddGrudge />
+      <Grudges />
     </>
   );
 }
