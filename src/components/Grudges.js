e020db8b2ea099adcc38f598fb4990a12c3dba23
Grace Lindelien <gracelindelien@gmail.com>
2021-03-05 09:56:32 -0800

    Create Grudges component

---
+++ src/components/Grudges.js
@@ -0,0 +1,11 @@
+import './Grudges.css';
+
+const Grudges = () => {
+  return (
+    <div className="Grudges">
+      Render Individual Grudge Components Here [List Form]
+    </div>
+  );
+};
+
+export default Grudges;
